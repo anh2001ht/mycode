469cb0c31f3291d727586cffffa100aa319154e5
HoangAnh <anh2001httttt@gmail.com>
2022-11-15 09:29:24 +0700

    hello world

---
+++ Solution1/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nhập tên: ");
+            
+            string name =Console.ReadLine();
+            Console.WriteLine("Nhập tuổi: ");
+            int age =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Tôi tên là {name}");
+            Console.WriteLine($"Năm nay tôi {age} tuổi");
+            Console.ReadKey();
+        }
+    }
+}
