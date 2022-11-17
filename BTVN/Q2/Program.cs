00e60f3867c3088dd3fa22a5062d7be90df714a1
HoangAnh <anh2001httttt@gmail.com>
2022-11-17 09:04:57 +0700

    done switch case

---
+++ BTVN/Q2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.UTF8;
+        string input = Console.ReadLine();
+        switch (input)
+        {
+            case "2":
+                Console.WriteLine("Thứ 2");
+                break;
+            case "3":
+                Console.WriteLine("Thứ 3");
+                break;
+            case "4":
+                Console.WriteLine("Thứ 4");
+                break;
+            case "5":
+                Console.WriteLine("Thứ 5");
+                break;
+            case "6":
+                Console.WriteLine("Thứ 6");
+                break;
+            case "7":
+                Console.WriteLine("Thứ 7");
+                break;
+            case "CN":
+                Console.WriteLine("Chủ nhật");
+                break;
+            default:
+                Console.WriteLine("Không phải ngày trong tuần");
+                break;
+        }
+        Console.ReadKey();
+
+    }
+}
