7eb6927c4497714465fdcc93ea13a195ef62a32e
HoangAnh <anh2001httttt@gmail.com>
2022-11-17 09:34:04 +0700

    done count event

---
+++ BTVN/Q3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.UTF8;
+
+        string[] arr = new string[] {
+            "Event-Assign", "Event-View", "Event-Assign", "Event-Acept",
+            "Event-Delete", "Event-Assign", "Event-Acept", "Event-Delete", "Event-Delete",
+            "Event-View", "Event-Assign", "Event-Acept", "Event-Delete", "Event-Assign"
+        };
+        int count = 0;
+
+        foreach (string s in arr)
+        {
+            if (s.Equals("Event-Acept"))
+            {
+                count++;
+            }
+        }
+
+        int countEvent = 0;
+        for (int i = 0; i < arr.Length - 3; i++)
+        {
+            if (arr[i].Equals("Event-View") && arr[i + 1].Equals("Event-Assign") &&
+                arr[i + 2].Equals("Event-Acept") && arr[i + 3].Equals("Event-Delete"))
+            {
+                countEvent++;
+            }
+        }
+        Console.WriteLine("Số lượng giao dịch: " + countEvent);
+        Console.WriteLine("Số lượng Event-Acept: " + count);
+
+
+        Console.ReadKey();
+        //123
+    }
+}
